dbe0d8834b1dafb08a392f405e0ada9130102dca
Yu Ho Yeon <zmfmsh46@gmail.com>
2024-02-19 14:59:28 +0900

    Spring-MVC structure

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") //스프링빈 이름을 url로 맞춤.
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
